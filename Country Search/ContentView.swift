8cabce2ccaf194ef0befe0245e101f6352b8bbb9
Anoop M <anoopm6@gmail.com>
2021-02-03 18:05:53 +0100

    Added tvOS target

---
+++ Country Search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Country Search
+//
+//  Created by Anoop M on 2021-02-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
